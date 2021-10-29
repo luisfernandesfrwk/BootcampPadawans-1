adf332c96cb5d27995749b43c7802594decbe155
cibele <cibelegoudinho@frwk.com.br>
2021-10-29 19:01:18 -0300

    Implementação modal Criar Eventos
    Co-authored-by: GabrielOliveiraFrame <gabrieloliveira@frwk.com.br>
    Co-authored-by: Thiago Savi <thiagosavi@frwk.com.br>
    Co-authored-by: Luis <luisfernandes@frwk.com.br>
    Co-authored-by: GDioz <gustavodiogenes@frwk.com.br>
    Co-authored-by: herculesoliverr <herculesoliveira@frwk.com.br>

--- src/components/create-event-modal/CreateEventModal.jsx
+++ src/components/create-event-modal/CreateEventModal.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 
 import "./CreateEventModal.css";
-import iconeNomeDoEvento from "../../assets/imagens/CriarEvento/name2-icon 1.png" 
+import iconeNomeDoEvento from "../../assets/imagens/CriarEvento/name2-icon 2.png"; 
+import iconeData from "../../assets/imagens/CriarEvento/data-icon 2.png";
+import iconeDoar from "../../assets/imagens/CriarEvento/doar-icon 2.png";
+import iconeFone from "../../assets/imagens/CriarEvento/fone-icon 3.png";
+import iconeHora from "../../assets/imagens/CriarEvento/Design sem nome 2.png";
+import iconeLocal from "../../assets/imagens/CriarEvento/place-icon 2.png";
+import iconeEmail from "../../assets/imagens/CriarEvento/email-icon 2.png";
+import iconeDescricao from "../../assets/imagens/CriarEvento/name-icon 2.png";
+import iconeCnpj from "../../assets/imagens/CriarEvento/cnpj2-icon 4.png";
 
 export default function CreateEventModal() {
     const [modal, setModal] = useState(false);
@@ -20,39 +28,61 @@ export default function CreateEventModal() {
                     <div className="modal-content">
                         <h2>Criar Evento</h2>
                         <div className="form-criar-evento">
-                        <label>
-                            <img src={iconeNomeDoEvento} alt="Nome do Evento" />
-                        </label>
-                        <input type="text" />
-                        <label>
-                            CNPJ
-                        </label>
-                        <input type="text" />
-                        <label>
-                            EMAIL
-                        </label>
-                        <input type="text" />
-                        <label>
-                            NUMERO
-                        </label>
-                        <input type="text" />
-                        <label>
-                            ENDEREÇO
-                        </label>
-                        <input type="text" />
-                        <label>
-                            FOTO
-                        </label>
-                        <input type="text" />
-                        <label>
-                            SENHA
-                        </label>
-                        <input type="text" />
-                        <label>
-                            CONFIRME SENHA
-                        </label>
-                        <input type="text" />
-
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeNomeDoEvento} alt="Nome do Evento" />
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeCnpj} alt="CNPJ" />
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeLocal} alt="Local" className = "local" />
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeHora} alt="horário"/>
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeData} alt="data"/>
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeEmail} alt="Email" />
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeFone} alt="telefone"/>
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeDoar} alt="doações"/>
+                                </label>
+                                <input type="text" />
+                            </div>
+                            <div className="formGroup">
+                                <label>
+                                <img src={iconeDescricao} alt="descrição"/>
+                                </label>
+                                <input type="text" />
+                            </div>
+                    
                         <button className="criar-conta">Criar Conta</button>
                         </div>
 
